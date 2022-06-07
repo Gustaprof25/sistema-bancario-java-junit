fc7e11e9b70752bd33d3c970afb5bb1c90a93bc3
Gustavo de Lima Pereira <gustaprof25@gmail.com>
2022-06-07 11:15:38 -0300

    Update ContaTest.java

--- src/test/java/com/sistemabancario/model/ContaTest.java
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -1,14 +1,27 @@
 package com.sistemabancario.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContaTest {
     
     @Test
-    void testAddMovimentacao() {
-        //TODO: Você precisa implementar este teste
+    void testSetNumeroValido() {
+        final Conta instance = new Conta();
+        final String esperado = "12345-6";
+        instance.setNumero(esperado);
+        final String obtido = instance.getNumero();
+        assertEquals(esperado, obtido);
+
     }
     
-    
+    @Test
+    void testSetNumeroInValidoExcecao(){
+        final Conta instance = new Conta();
+        final String invalido = "123";
+        assertThrows(IllegalArgumentException.class, () -> instance.setNumero(invalido));
+    }
 }
